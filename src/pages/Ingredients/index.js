6eeed10fb6325b452e5eaa71e9e3ac71e1069b93
Benjamin Cai <benjamincaiyh@gmail.com>
2020-04-05 15:11:06 +0800

    Rename the major page

--- src/pages/Ingredients/index.js
+++ src/pages/Ingredients/index.js
@@ -1,6 +1,67 @@
 import React from 'react';
-import DefaultIngredients from './Ingredients';
+import { Row, Container, Col, Card, CardBody, CardTitle, Button, ListGroup, ListGroupItem, Input } from 'reactstrap';
 
 export default function Ingredients() {
-    return <DefaultIngredients />;
+    //     this.state = {
+    //         user: {},
+    //         ingredients: [],
+    //         isAuthenticated: false,
+    //         inventory: [],
+    //         filtered: [],
+    //         search: ''
+    //     };
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Row>
+                        <Col>
+                            <div className="page-title-box">
+                                <h4 className="page-title">Ingredient Box</h4>
+                            </div>
+                        </Col>
+                        {/* <Col>
+                                    <div className="page-title-box">
+                                        <input type="text" className="input" onChange={(e) => this.updateSearch(e)} value={this.state.search} placeholder="Search..." />
+                                    </div>
+                                </Col> */}
+                    </Row>
+                    <Row>
+                        <Input type="text" className="input" onChange={e => {}} value={''} placeholder="Search..." />
+                        <div className="container">
+                            <ListGroup flush>
+                                {[].map((ingredient, index) => (
+                                    <ListGroupItem>
+                                        <strong>{ingredient.name}</strong>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-plus" />
+                                        </Button>
+                                    </ListGroupItem>
+                                ))}
+                            </ListGroup>
+                        </div>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row>
+                        <div className="page-title-box">
+                            <h4 className="page-title">My Fridge</h4>
+                        </div>
+                    </Row>
+                    <ListGroup flush>
+                        {[].map((ingredient, index) => (
+                            <ListGroupItem>
+                                <strong>{ingredient.name}</strong>
+                                <span style={{ float: 'right' }}>
+                                    <Button size="sm" onClick={() => {}}>
+                                        <i className="fa fa-trash-alt" />
+                                    </Button>
+                                </span>
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
--- src/pages/Planner/index.js
+++ src/pages/Planner/index.js
@@ -1,6 +1,268 @@
 import React from 'react';
-import DefaultPlanner from './Planner';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {
+    Row,
+    Col,
+    Card,
+    CardBody,
+    CardImg,
+    CardText,
+    CardTitle,
+    CardDeck,
+    Container,
+    Collapse,
+    CardHeader,
+    CardFooter,
+    CardSubtitle,
+    Button,
+    ListGroup,
+    ListGroupItem,
+    UncontrolledButtonDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
 
 export default function Planner() {
-    return <DefaultPlanner />;
+    // this.state = {
+    //     user: {},
+    //     monday: [],
+    //     tuesday: [],
+    //     wednesday: [],
+    //     thursday: [],
+    //     friday: [],
+    //     saturday: [],
+    //     sunday: [],
+    //     grocery: []
+    // };
+
+    //     removeSunday(rec_id) {
+    //     let token = window.localStorage.getItem('accessJWT');
+    //     const url = `/api/users/${token}/planner/sunday/${rec_id}`;
+    //     axios.delete(url, { withCredentials: true })
+    //     .then(r => {
+    //         let sunday = this.state.sunday;
+    //         const removeIndex = sunday.map(item => item.spoon_id).indexOf(rec_id);
+    //         sunday.splice(removeIndex, 1);
+    //         this.setState({ sunday: sunday });
+    //         console.log(r.status);
+    //         console.log(this.state.sunday)
+    //     })
+    //     .catch(e => console.log(e))
+    // }
+
+    // getGrocery() {
+    //     let token = window.localStorage.getItem('accessJWT');
+    //     const url = `/api/users/${token}/planner/grocery`;
+    //     axios.get(url, { withCredentials: true })
+    //     .then(response => response.data)
+    //     .then((data) => {
+    //         console.log(data)
+    //         this.setState({ grocery: data })
+    //         console.log(this.state.grocery)
+    //     })
+
+    // }
+
+    return (
+        <Container>
+            <Row>
+                <Col lg={12}>
+                    <div className="page-title-box">
+                        <h3 className="page-title">Planner</h3>
+                    </div>
+                </Col>
+            </Row>
+
+            <h4>Monday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Tuesday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Wednesday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Thursday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Friday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Saturday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+
+            <h4>Sunday</h4>
+            <Row className="bg-picture card-box">
+                <CardDeck>
+                    {[].map(recipe => (
+                        <Col>
+                            <Card key={recipe.spoon_id}>
+                                <CardImg top width="100%" src={recipe.imageUrl} />
+                                <CardBody>
+                                    <CardTitle>
+                                        <h4>
+                                            <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
+                                                {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                            </Link>
+                                        </h4>
+                                    </CardTitle>
+                                    <span style={{ float: 'right' }}>
+                                        <Button size="sm" onClick={() => {}}>
+                                            <i className="fa fa-trash-alt" />
+                                        </Button>
+                                    </span>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    ))}
+                </CardDeck>
+            </Row>
+        </Container>
+    );
 }
