56e6225ac5a7e9d974daf0244367f926b8fefa41
Benjamin Cai <benjamincaiyh@gmail.com>
2020-04-06 13:24:33 +0800

    Recipe search and recipe start rating

--- src/pages/Recipes/RecipeCard.js
+++ src/pages/Recipes/RecipeCard.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Card, CardBody, CardImg, CardText, CardTitle, CardHeader, CardFooter, CardSubtitle } from 'reactstrap';
+import Icon from '@mdi/react';
+import { mdiTimer, mdiAccount, mdiStar } from '@mdi/js';
 import './recipeCard.scss';
 
 export default function RecipeCard({ recipe }) {
     const { spoon_id: spoonId, imageUrl, title, servings, readyInMinutes } = recipe;
+
+    const StarRating = () => [...Array(5).keys()].map(k => <Icon key={k} path={mdiStar} color="#FFC741" size={1} />);
+    const ServingIcon = () => <Icon path={mdiAccount} color="#909090" size={1} title="Number of serving" />;
+    const TimerIcon = () => <Icon path={mdiTimer} color="#909090" size={1} title="Timer for cooking" />;
+
     return (
         <Card className="recipe-card mb-4">
             <Link to={`/recipe/${spoonId}`}>
@@ -13,10 +20,21 @@ export default function RecipeCard({ recipe }) {
             </Link>
             <CardBody className="p-4 recipe-card-body">
                 <Link to={`/recipe/${spoonId}`}>
-                    <CardTitle tag="h3">{title.length < 15 ? title : `${title.substring(0, 25)}...`}</CardTitle>
+                    <CardTitle className="mb-2 pb-2" tag="h3">
+                        {title.length < 15 ? title : `${title.substring(0, 25)}...`}
+                    </CardTitle>
                 </Link>
-                <CardText>{`${servings} servings`}</CardText>
-                <CardText>{`${readyInMinutes} min`}</CardText>
+                <CardText>
+                    <StarRating />
+                </CardText>
+                <CardText className="recipe-card-text">
+                    <ServingIcon />
+                    {` ${servings} servings`}
+                </CardText>
+                <CardText className="recipe-card-text">
+                    <TimerIcon />
+                    {` ${readyInMinutes} min`}
+                </CardText>
             </CardBody>
         </Card>
     );
--- src/pages/Recipes/index.js
+++ src/pages/Recipes/index.js
@@ -1,93 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row, Col, CardDeck, Collapse, Button } from 'reactstrap';
+import { Container, Row, Col, Input } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRecipes } from '@frontend/redux/actions';
 import RecipeCard from './RecipeCard';
 
 export default function Recipe() {
-    //     // this.state = {
-    //     //     user: {},
-    //     //     query: '',
-    //     //     message: '',
-    //     //     allRecipes: [],
-    //     //     pageOfItems: [],
-    //     //     isAuthenticated: false
-    //     // };
-
-    //     this.onChangePage = this.onChangePage.bind(this);
-    //     this.handleOnInputChange = this.handleOnInputChange.bind(this);
     const recipes = useSelector(state => state.recipes);
     const dispatch = useDispatch();
     const [searchBarValue, setSearchBarValue] = useState('');
+    const [localRecipes, updateLocalRecipes] = useState(recipes);
 
     useEffect(() => {
         dispatch(fetchRecipes());
     }, []);
 
+    useEffect(() => {
+        updateLocalRecipes(recipes);
+    }, [recipes]);
+
+    useEffect(() => {
+        if (searchBarValue) {
+            const kwGroup = searchBarValue.toLowerCase().split(' ');
+            const filteredRecipes = recipes.filter(r => kwGroup.some(kw => r.title.toLowerCase().includes(kw)));
+            updateLocalRecipes(filteredRecipes);
+        } else if (localRecipes.length !== recipes.length) {
+            updateLocalRecipes(recipes);
+        }
+    }, [searchBarValue]);
+
     return (
         <Container>
-            <Row>
+            <Row className="mb-5">
                 <Col>
                     <div className="page-title-box">
                         <h1 className="page-title">My Recipes</h1>
                     </div>
-                    <input
+                    <Input
+                        id="recipes-search-bar"
                         type="text"
-                        className="form-control"
-                        name="query"
-                        value={''}
-                        id="search-input"
-                        placeholder="Search..."
-                        onChange={() => {}}
+                        onChange={e => setSearchBarValue(e.target.value)}
+                        value={searchBarValue}
+                        placeholder="Search the recipe you want..."
+                        bsSize="lg"
+                        className="recipes-serach-bar"
                     />
                 </Col>
             </Row>
 
             <Row>
-                {recipes.map(recipe => (
-                    <Col sm="12" md="3" key={recipe.spoon_id}>
+                {localRecipes.map(recipe => (
+                    <Col xs="12" md="6" lg="3" key={recipe.spoon_id}>
                         <RecipeCard recipe={recipe} />
                     </Col>
                 ))}
             </Row>
-            {/* {recipes.map((recipe, index) => (
-                        <div className="col-3">
-                            <div className="card">
-                                <div className="thumbnail-holder">
-                                    <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
-                                        <img src={recipe.imageUrl} alt="" />
-                                        <div className="hover-cover" />
-                                        <div className="hover-icon">View Recipe</div>
-                                    </Link>
-                                </div>
-
-                                <div className="recipe-box-content">
-                                    <h3>
-                                        <Link to={{ pathname: `/recipe/?spoon_id=${recipe.spoon_id}`, state: { recipe } }}>
-                                            {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
-                                        </Link>
-                                    </h3>
-
-                                    <div className="rating five-stars">
-                                        <div className="star-rating" />
-                                        <div className="star-bg" />
-                                    </div>
-
-                                    <div className="recipe-meta">
-                                        <i className="fa fa-user" />
-                                        {recipe.servings} servings
-                                    </div>
-                                    <div className="recipe-meta">
-                                        <i className="fa fa-clock-o" />
-                                        {recipe.readyInMinutes} min
-                                    </div>
-
-                                    <div className="margin-bottom-40" />
-                                </div>
-                            </div>
-                        </div>
-                    ))} */}
         </Container>
     );
 }
